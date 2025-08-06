4bc3983c1f42ddf3f715850f0124ad1d83fa1c8b
RafaelGalazzi <rafaelgalazzi@ucl.br>
2025-08-06 19:08:47 -0300

    added auth and users modules

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+
+describe('UsersController', () => {
+  let usersController: UsersController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsersService],
+    }).compile();
+
+    usersController = app.get<UsersController>(UsersController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(usersController.getHello()).toBe('Hello World!');
+    });
+  });
+});

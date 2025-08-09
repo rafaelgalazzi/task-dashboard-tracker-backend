c65435a56545e0bd15eb69bcbb89c9d609ecf353
RafaelGalazzi <rafaelgalazzi@ucl.br>
2025-08-09 18:16:43 -0300

    added cookies and login api

--- src/domain/users/users.controller.spec.ts
+++ src/domain/users/users.controller.spec.ts
@@ -1,19 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { DatabaseModule } from 'src/adapters/database.module';
-import { HashModule } from 'src/adapters/hash.module';
-import { UsersRepository } from './users.repository';
 
 describe('UsersController', () => {
   let usersController: UsersController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [DatabaseModule, HashModule],
       controllers: [UsersController],
-      providers: [UsersService, UsersRepository],
-      exports: [UsersRepository],
+      providers: [UsersService],
     }).compile();
 
     usersController = app.get<UsersController>(UsersController);
